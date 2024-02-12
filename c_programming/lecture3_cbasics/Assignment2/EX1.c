858d7a477dc05e236ab4087ccb0a3221b34d8ce2
Karim Aboualenin <karim72727272@yahoo.com>
2024-02-12 20:30:07 +0200

    solving EX1 in assignment 2

---
+++ c_programming/lecture3_cbasics/Assignment2/EX1.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+ {	
+ int num;
+ printf("enter an intger to check if it's odd or even");
+ fflush(stdin);fflush(stdout);
+ scanf("%d",&num);
+ if (num%2 == 0)
+	 printf("%d is even",num);
+ else
+	 printf("%d is odd",num);
+}
